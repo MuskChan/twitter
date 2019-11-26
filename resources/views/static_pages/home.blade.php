55dfee6120b9a1a319e05052668170fb431eeda1
MuskChan <523919565@qq.com>
2019-11-26 14:56:00 +0800

    init

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.default')
+
+@section('content')
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h4>twitter List</h4>
+        <hr>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+        <section class="stats mt-2">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h4>twitter List</h4>
+        <hr>
+        @include('shared._feed')
+      </div>
+    </div>
+  @endif
+@stop
